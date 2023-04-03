f320afbb95a900ab1ec7425752449585df6533fb
Muammad Tayyab Khan <112394525+tayyabkhan5614@users.noreply.github.com>
2023-04-03 23:01:50 +0500

    Delete Depression-Tester directory

--- Depression-Tester/result.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("result").innerHTML=localStorage.getItem("Sad") ;
-
-// var x = localStorage.getItem("mytime");
-// document.getElementById("demo").innerHTML = x;
-// document.getElementById("num").innerHTML=localStorage.getItem();
-
-    // var myArray = localStorage.getItem("myValue");
-    // console.log(myArray);
